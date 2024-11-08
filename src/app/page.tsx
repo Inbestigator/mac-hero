edca988ecb2271bd37b4ef68d8eddbc11037eb90
Inbestigator <inbestigatoryt@gmail.com>
2024-11-08 08:43:02 -0800

    Synced Text scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
     const video = videoRef.current;
 
     const scrollProgress = Math.min(
-      window.scrollY / (window.innerHeight * 5 - window.innerHeight),
+      window.scrollY / (window.innerHeight * 5.25),
       1
     );
 
@@ -28,48 +28,50 @@ export default function Hero() {
 
     return () => window.removeEventListener("scroll", scrollHandler);
   }, []);
-  console.log(progress);
 
   return (
-    <div className="h-[500vh]">
-      <div className="sticky flex flex-col items-center justify-center top-0 left-0 h-screen w-full overflow-hidden">
-        <div className="absolute size-full overflow-hidden flex items-center justify-center">
-          <video
-            ref={videoRef}
-            data-video-progress-kf-l1='{"start": "lerp(0.0, a0t, a0b - a1h)", "end": "lerp(0.085, a0t, a0b - a1h)", "progress": [0.02, 0.13 ], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content"], "breakpointMask": "L"}'
-            data-video-progress-kf-l2='{"start": "lerp(0.085, a0t, a0b - a1h)", "end": "lerp(0.325, a0t, a0b - a1h)", "progress": [0.13, 0.24 ], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content"], "breakpointMask": "L"}'
-            data-video-progress-kf-l3='{"start": "lerp(0.325, a0t, a0b - a1h)", "end": "lerp(0.47, a0t, a0b - a1h)", "progress": [0.24, 0.5 ], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content"], "breakpointMask": "L"}'
-            data-video-progress-kf-l4='{"start": "lerp(0.47, a0t, a0b - a1h)", "end": "a0b - a2h - 119px + 50vh", "progress": [0.5, 1.0], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content", ".hero-copy-wrapper"], "breakpointMask": "L"}'
-            data-video-progress-kf-m1='{"start": "lerp(0.0, a0t, a0b - a1h)", "end": "lerp(0.085, a0t, a0b - a1h)", "progress": [0.02, 0.13 ], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content"], "breakpointMask": "M"}'
-            data-video-progress-kf-m2='{"start": "lerp(0.085, a0t, a0b - a1h)", "end": "lerp(0.325, a0t, a0b - a1h)", "progress": [0.13, 0.24 ], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content"], "breakpointMask": "M"}'
-            data-video-progress-kf-m3='{"start": "lerp(0.325, a0t, a0b - a1h)", "end": "lerp(0.47, a0t, a0b - a1h)", "progress": [0.24, 0.5 ], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content"], "breakpointMask": "M"}'
-            data-video-progress-kf-m4='{"start": "lerp(0.47, a0t, a0b - a1h)", "end": "a0b - 100px + 50vh", "progress": [0.5, 1.0], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content", ".hero-copy-wrapper"], "breakpointMask": "M"}'
-            data-video-progress-kf-s1='{"start": "lerp(0.0, a0t, a0b - a1h)", "end": "lerp(0.2, a0t, a0b - a1h)", "progress": [0.02, 0.13 ], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content"], "breakpointMask": "S"}'
-            data-video-progress-kf-s2='{"start": "lerp(0.2, a0t, a0b - a1h)", "end": "lerp(0.55, a0t, a0b - a1h)", "progress": [0.13, 0.24 ], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content"], "breakpointMask": "S"}'
-            data-video-progress-kf-s3='{"start": "lerp(0.55, a0t, a0b - a1h)", "end": "lerp(0.7, a0t, a0b - a1h)", "progress": [0.24, 0.5 ], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content"], "breakpointMask": "S"}'
-            data-video-progress-kf-s4='{"start": "lerp(0.7, a0t, a0b - a1h)", "end": "a0b - 100px + 100vh", "progress": [0.5, 1], "easeFunction": "easeOutQuad", "anchors": [".sticky-container", ".sticky-content", ".hero-copy-wrapper"], "breakpointMask": "S"}'
-            data-video-load-kf='{"start": "a0t-100vh", "end": "a0b + 100vh", "anchors": [".sticky-container"]}'
-            data-video-basepath="/105/media/us/macbook-air/2024/abecf8fa-b944-4698-94ce-14616e166bff/anim/hero/"
-            data-anim-keyframe='{"start": "a0t-100vh", "end": "a0b + 100vh", "anchors": [".sticky-container"], "cssClass": "js-will-change", "toggle": true, "disabledWhen": ["no-enhance-xp"]}'
-            playsInline
-            muted
-            src="/large.webm"
-            aria-hidden
-          />
-          <h1
-            className="text-[9rem] font-black absolute size-full flex justify-center items-center"
-            style={{ opacity: 1 - progress * 35, scale: 1 - progress * 20 }}
+    <>
+      <div className="h-[500vh] sticky-container">
+        <div className="sticky flex flex-col items-center justify-center top-0 left-0 h-screen w-full overflow-hidden sticky-content">
+          <div className="absolute size-full overflow-hidden flex items-center justify-center">
+            <video
+              ref={videoRef}
+              data-video-progress-kf-l1='{"start": "lerp(0.0, a0t, a0b - a1h)", "end": "lerp(0.085, a0t, a0b - a1h)", "progress": [0.02, 0.13 ], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content"], "breakpointMask": "L"}'
+              data-video-progress-kf-l2='{"start": "lerp(0.085, a0t, a0b - a1h)", "end": "lerp(0.325, a0t, a0b - a1h)", "progress": [0.13, 0.24 ], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content"], "breakpointMask": "L"}'
+              data-video-progress-kf-l3='{"start": "lerp(0.325, a0t, a0b - a1h)", "end": "lerp(0.47, a0t, a0b - a1h)", "progress": [0.24, 0.5 ], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content"], "breakpointMask": "L"}'
+              data-video-progress-kf-l4='{"start": "lerp(0.47, a0t, a0b - a1h)", "end": "a0b - a2h - 119px + 50vh", "progress": [0.5, 1.0], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content", ".hero-copy-wrapper"], "breakpointMask": "L"}'
+              data-video-progress-kf-m1='{"start": "lerp(0.0, a0t, a0b - a1h)", "end": "lerp(0.085, a0t, a0b - a1h)", "progress": [0.02, 0.13 ], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content"], "breakpointMask": "M"}'
+              data-video-progress-kf-m2='{"start": "lerp(0.085, a0t, a0b - a1h)", "end": "lerp(0.325, a0t, a0b - a1h)", "progress": [0.13, 0.24 ], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content"], "breakpointMask": "M"}'
+              data-video-progress-kf-m3='{"start": "lerp(0.325, a0t, a0b - a1h)", "end": "lerp(0.47, a0t, a0b - a1h)", "progress": [0.24, 0.5 ], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content"], "breakpointMask": "M"}'
+              data-video-progress-kf-m4='{"start": "lerp(0.47, a0t, a0b - a1h)", "end": "a0b - 100px + 50vh", "progress": [0.5, 1.0], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content", ".hero-copy-wrapper"], "breakpointMask": "M"}'
+              data-video-progress-kf-s1='{"start": "lerp(0.0, a0t, a0b - a1h)", "end": "lerp(0.2, a0t, a0b - a1h)", "progress": [0.02, 0.13 ], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content"], "breakpointMask": "S"}'
+              data-video-progress-kf-s2='{"start": "lerp(0.2, a0t, a0b - a1h)", "end": "lerp(0.55, a0t, a0b - a1h)", "progress": [0.13, 0.24 ], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content"], "breakpointMask": "S"}'
+              data-video-progress-kf-s3='{"start": "lerp(0.55, a0t, a0b - a1h)", "end": "lerp(0.7, a0t, a0b - a1h)", "progress": [0.24, 0.5 ], "easeFunction": "linear", "anchors": [".sticky-container", ".sticky-content"], "breakpointMask": "S"}'
+              data-video-progress-kf-s4='{"start": "lerp(0.7, a0t, a0b - a1h)", "end": "a0b - 100px + 100vh", "progress": [0.5, 1], "easeFunction": "easeOutQuad", "anchors": [".sticky-container", ".sticky-content", ".hero-copy-wrapper"], "breakpointMask": "S"}'
+              data-video-load-kf='{"start": "a0t-100vh", "end": "a0b + 100vh", "anchors": [".sticky-container"]}'
+              data-video-basepath="/large.webm"
+              data-anim-keyframe='{"start": "a0t-100vh", "end": "a0b + 100vh", "anchors": [".sticky-container"], "cssClass": "js-will-change", "toggle": true, "disabledWhen": ["no-enhance-xp"]}'
+              playsInline
+              muted
+              src="/large.webm"
+              aria-hidden
+            />
+            <h1
+              className="text-[9rem] font-black absolute size-full flex justify-center items-center"
+              style={{ opacity: 1 - progress * 35, scale: 1 - progress * 20 }}
+            >
+              MacBook Air
+            </h1>
+          </div>
+          <div
+            className="flex-col max-w-[60rem] gap-10 absolute top-[calc(100vh-300px)] hero-copy-wrapper"
+            style={{ display: progress > 0.77 ? "flex" : "none" }}
           >
-            MacBook Air
-          </h1>
-        </div>
-        <div
-          className="flex-col max-w-[60rem] gap-10 absolute top-[calc(100vh-300px)]"
-          style={{ display: progress > 0.81 ? "flex" : "none" }}
-        >
-          <Text />
+            <Text />
+          </div>
         </div>
       </div>
-    </div>
+      <div className="h-[calc(50vh+300px)]" />
+    </>
   );
 }
